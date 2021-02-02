d69ea0c2be8481f69b0eb5772b57944d1d505905
Your Name <j.hamwijk34@gmail.com>
2021-02-02 14:19:23 +0100

    Update HelloController.java

--- server/src/main/java/com/example/springazurecloud/HelloController.java
+++ server/src/main/java/com/example/springazurecloud/HelloController.java
@@ -10,11 +10,11 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class HelloController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Welcome to my Deployed app on Azure, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Deployed by Jordy Coder") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
